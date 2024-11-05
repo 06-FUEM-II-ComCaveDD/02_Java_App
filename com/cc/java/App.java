eea30f53fba196a46f603dbaace3fac9d3cf3460
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-11-05 10:01:30 +0000

    01 / try _ catch() test

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,10 +1,42 @@
 package com.cc.java;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+
 public class App 
 {
+    // Argumente für Connection-Parameter
+    static String conURL = "jdbc:mysql://104.248.39.169:3306/mydb";
+    static String usrStr = "remote_user";
+    static String usrPwd = "pass@remote";
+
+    static Connection con;
+    // static Statement stmt;
+    static ResultSet rs;
+
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        
+      try {
+        
+          // Load SQL Server JDBC driver and establish connection.
+          System.out.print("Connecting to SQL Server ... ");
+
+
+      } catch (Exception e) {
+
+        // TODO: handle exception
+
+
+      }
+
+
+
+
+
+
+
+
     }
     
 }
